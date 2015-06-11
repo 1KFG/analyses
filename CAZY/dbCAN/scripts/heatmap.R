f843b2d5b0b81762adb4f4fdaebf88801f9102d6
Jason Stajich <jason@bioperl.org>
2015-06-11 16:08:00 -0700

    handle 2 domains with no numbers

---
+++ CAZY/dbCAN/scripts/heatmap.R
@@ -0,0 +1,45 @@
+library(gplots)
+library(fastcluster)
+library(RColorBrewer)
+library(colorRamps)
+library(pheatmap)   
+#library(amap)
+library(ape)
+
+palette <- colorRampPalette(c('blue','white','red'))(100)
+#palette <- greenred(100)
+
+pdf("CAZY_1KFG.pdf",height=25,width=25)
+
+cazy <- read.table("cazy_sum.dat",header=T,sep="\t",row.names=1);
+gm <- data.matrix(cazy)
+
+ch <- 5
+cw <- 5
+
+#head(gm)
+fs_row = 6
+fs_col = 5
+#res <- pheatmap(head(gm,30), main="CAZY", fontsize_row = fs_row,
+#         fontsize_column = fs_col,
+#         cluster_cols = TRUE, cluster_rows = TRUE,
+#         col = palette, scale="row",
+#         cellheight = ch,
+#         cellwidth  = cw,
+#         legend = T,
+#                                        # Rowv=NA,Colv=NA,
+#         );
+
+gmt <- t(gm)
+res_t <- pheatmap(gmt, main="CAZY", fontsize_row = fs_row,
+         fontsize_col = fs_col,
+         cluster_cols = TRUE, cluster_rows = TRUE,
+         col = palette, scale="column",
+         cellheight = ch,
+         cellwidth  = cw,
+         legend = T,
+                                        # Rowv=NA,Colv=NA,
+         );
+
+#cutree(res_t$tree_row, k = 10)
+#data.frame(cluster = cutree(res_t$tree_row, k = 10))
--- CAZY/dbCAN/scripts/make_cazy_sum.pl
+++ CAZY/dbCAN/scripts/make_cazy_sum.pl
@@ -1,8 +1,10 @@
 #!env perl
+use List::Util qw(sum);
 use strict;
 use warnings;
 use File::Spec;
 
+my $min_count = 20;
 my @files = @ARGV;
 my %domains;
 my @species;
@@ -29,10 +31,14 @@ for my $domain ( map { $_->[0] }
 			     ($a->[2] eq 'NC' ? (1) : 
 			      $b->[2] eq 'NC' ? (-1) : $a->[2] <=> $b->[2]) }
 		 map { my ($val,$n) = ($_ =~ /(\D+)(\d+|NC)/);
-		       #warn("val is $val n is $n\n");
-		       #if( ! $val ) { warn("key was $_\n") }
+		       if( ! defined $val ) { warn("No val for $_\n"); 
+					     ($val,$n) = ($_, 1); }
 		       [$_,$val,$n] }
 		 keys %domains ) {
+    my $sum = sum(map { exists $domains{$domain}->{$_} ? 
+					$domains{$domain}->{$_} : '0' } 
+		  @species);
+    next if( $sum < $min_count);
     print join("\t", $domain, map { exists $domains{$domain}->{$_} ? 
 					$domains{$domain}->{$_} : '0' } 
 	       @species),"\n";
