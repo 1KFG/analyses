fbb3d8d848314913dfdfd65a3b75a9691cf9fc9e
Jason Stajich <jason@bioperl.org>
2016-07-22 17:55:20 -0700

    move old files out for cleanup

--- Phylogenomics/jobs/make_unaln.JGI1086_trim.sh
+++ Phylogenomics/jobs/make_unaln.JGI1086_trim.sh
@@ -1,4 +1,4 @@
-#PBS -N makeUaln.JGI_1086 -j oe -l walltime=8:00:00,mem=4gb
+#PBS -N makeUaln.JGI_1086_trim -j oe -l walltime=8:00:00,mem=8gb
 #which perl
 # DUDUE DON"T RUN THIS AS ARRAY JOBS!!
 module load perl
