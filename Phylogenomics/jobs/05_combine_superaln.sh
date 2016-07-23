d217314e6c4b849757c073f6c2f5b16ee287266d
Jason Stajich <jason@bioperl.org>
2016-07-22 18:05:11 -0700

    silence debugging

--- Phylogenomics/jobs/05_combine_superaln.sh
+++ Phylogenomics/jobs/05_combine_superaln.sh
@@ -1,9 +1,11 @@
-#PBS -l nodes=1:ppn=1 -j oe -N combineAll
+#PBS -l nodes=1:ppn=1 -j oe -N combineAll:wq
+
 
 if [ ! -f expected ]; then
  bash jobs/make_expected_file.sh
 fi
 EXPECTEDNAMES=expected
 MARKERS=JGI_1086
-count=`wc -l $EXPECTEDNAMES`
-perl scripts/combine_fasaln.pl -o all_${count}.${MARKERS}.fasaln -of fasta -d aln/$MARKERS -expected $EXPECTEDNAMES
+count=`wc -l $EXPECTEDNAMES | awk '{print $1}'`
+#echo "count is $count"
+perl scripts/combine_fasaln.pl -o all_${count}.${MARKERS}.fasaln -of fasta -d aln/$MARKERS -expected $EXPECTEDNAMES > all_${count}.${MARKERS}.partitions.txt
