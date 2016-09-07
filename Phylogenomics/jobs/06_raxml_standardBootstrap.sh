0098b724af22be64aede61585aff06d9922d9243
Jason Stajich <jason@bioperl.org>
2016-09-07 11:24:40 -0700

    use vars  - later set this with cfg file

--- Phylogenomics/jobs/06_raxml_standardBootstrap.sh
+++ Phylogenomics/jobs/06_raxml_standardBootstrap.sh
@@ -6,15 +6,17 @@ CPU=2
 if [ $PBS_NUM_PPN ]; then
  CPU=$PBS_NUM_PPN
 fi
+PRE=1KFG
+OUT=Pult
 count=`wc -l expected | awk '{print $1}'`
 datestr=`date +%Y_%b_%d`
-str=Zygo.$datestr".JGI1086".${count}sp
+str=$PRE.$datestr".JGI1086".${count}sp
 IN=all_${count}.JGI_1086
 if [ ! -f phylo/$str.fasaln ]; then
  cp $IN.fasaln phylo/$str.fasaln
  cp $IN.partitions.txt phylo/$str.partitions
 fi
 cd phylo
-PREFIX=Zygo_RD2_Standard.$str
-raxmlHPC-PTHREADS-AVX -T $CPU -f a -x 227 -p 771 -o Rall -m PROTGAMMAAUTO \
-  -s $str.fasaln -n $PREFIX -N autoMRE 
+PREFIX=Standard.$str
+raxmlHPC-PTHREADS-AVX -T $CPU -f a -x 227 -p 771 -o $OUT -m PROTGAMMALG \
+  -s $str.fasaln -n $PREFIX -N autoMRE -d
