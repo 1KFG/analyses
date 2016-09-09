c6c87e65fd18202910515859bf9be66a8058ae93
Jason Stajich <jason@bioperl.org>
2016-09-08 23:32:39 -0700

    denovo aln processing

--- Phylogenomics/jobs/05-2_denovoaln_combine_superaln.sh
+++ Phylogenomics/jobs/05-2_denovoaln_combine_superaln.sh
@@ -8,5 +8,11 @@ EXPECTEDNAMES=expected
 MARKERS=JGI_1086
 count=`wc -l $EXPECTEDNAMES | awk '{print $1}'`
 #echo "count is $count"
-perl scripts/combine_fasaln.pl -ext autotrim -o all_${count}.denovo.${MARKERS}.fasaln -of fasta -d aln/$MARKERS -expected $EXPECTEDNAMES > all_${count}.denovo.${MARKERS}.partitions.txt
+perl scripts/combine_fasaln.pl -ext denovo.trim -o all_${count}.denovo.${MARKERS}.fasaln -of fasta -d aln/$MARKERS -expected $EXPECTEDNAMES > all_${count}.denovo.${MARKERS}.partitions.txt
 convertFasta2Phylip.sh all_${count}.denovo.${MARKERS}.fasaln > all_${count}.denovo.${MARKERS}.phy
+
+ctCDS=$(ls aln/$MARKERS/*.denovo_cdsaln.trim | wc -l | awk '{print $1}')
+if [ $ctCDS -gt 0 ]; then
+perl scripts/combine_fasaln.pl -o all_${count}.${MARKERS}.denovocds.fasaln -ext denovo_cdsaln.trim -if fasta -of fasta -d aln/$MARKERS -expected $EXPECTEDNAMES > all_${count}.${MARKERS}.cds.partitions.txt
+convertFasta2Phylip.sh all_${count}.${MARKERS}.denovocds.fasaln > all_${count}.${MARKERS}.denovocds.phy
+fi
