265c9355c6b849f31e9cc9715cf37e3519180cff
Jason Stajich <jason@bioperl.org>
2016-10-03 13:04:28 -0700

    cdbyank

--- Phylogenomics/scripts/construct_unaln_files_cdbfasta.pl
+++ Phylogenomics/scripts/construct_unaln_files_cdbfasta.pl
@@ -5,9 +5,10 @@ use Getopt::Long;
 use File::Spec;
 my $dir = 'search';
 my $dbdir = 'pep';
-my $ext = "fasta";
+my $ext = "fa";
 my $outdir = 'aln';
 my $idxfile = 'allseq';
+my $MIN_COUNT = 4;
 
 GetOptions(
     'd|dir:s'   => \$dir,
@@ -15,12 +16,15 @@ GetOptions(
     'I|idx:s'   => \$idxfile,
     'db:s'      => \$dbdir,
     'o|out:s'   => \$outdir,
+    'm|min:i'   => \$MIN_COUNT, # minimum number of sp in a gene cluster to use it, otherwise skip it
     );
 $idxfile = File::Spec->catfile($dbdir,$idxfile);
-`cat $dbdir/*.$ext | esl-reformat fasta - > $idxfile`;
-`cdbfasta $idxfile`;
+if( ! -f $idxfile ) {
+ `cat $dbdir/*.$ext | esl-reformat fasta - > $idxfile`;
+ `cdbfasta $idxfile`;
+}
 
-opendir(BEST,$dir) || die $dir;
+opendir(BEST,$dir) || die "cannot open dir: $dir, $!";
 
 my %by_gene;
 for my $file ( readdir(BEST) ) {
@@ -33,7 +37,12 @@ for my $file ( readdir(BEST) ) {
     }
 }
 for my $gene ( keys %by_gene ) {
-    open(CDBYANK,"| cdbyank $idxfile.cidx -o $outdir/$gene.fa") || die $!;    
+ my $ct = scalar keys %{$by_gene{$gene}};
+    if ( $ct < $MIN_COUNT ) {
+     warn("skipping $gene has only $ct genes\n");
+     next;
+    }
+    open(CDBYANK,"| cdbyank $idxfile.cidx -o $outdir/$gene.$ext") || die $!;    
     while( my ($sp,$seqname) = each %{$by_gene{$gene}} ) {
 	print CDBYANK $seqname,"\n";
     }
