c1a7ca36def80564be4a24173b51792c24bed0a6
Jason Stajich <jason@bioperl.org>
2017-01-12 00:24:02 -0800

    slurm transition

--- Phylogenomics/jobs/06_raxml_RELL.sh
+++ Phylogenomics/jobs/06_raxml_RELL.sh
@@ -1,7 +1,23 @@
-#PBS -l nodes=1:ppn=32 -q batch -N raxmlRELL -j oe
+#!/usr/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=64
+#SBATCH --mem-per-cpu=1G
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=raxml.RELL
+
 module load RAxML
 
 CPU=2
+if [ $SLURM_JOB_CPUS_PER_NODE ]; then
+ CPU=$SLURM_JOB_CPUS_PER_NODE
+fi
+
+if [ $PBS_NUM_PPN ]; then
+ CPU=$PBS_NUM_PPN
+fi
+
+# -l nodes=1:ppn=32 -q batch -N raxmlRELL -j oe
+
 if [ $PBS_NUM_PPN ]; then
  CPU=$PBS_NUM_PPN
 fi
@@ -9,7 +25,6 @@ if [ -f config.txt ]; then
  source config.txt
 else
  PREFIX=ALL
- FINALPREF=1KFG
  OUT=Pult
 fi
 count=`wc -l expected | awk '{print $1}'`
@@ -23,4 +38,4 @@ fi
 
 cd phylo
 raxmlHPC-PTHREADS-AVX -T $CPU -f D -p 771 -o $OUT -m PROTGAMMALG \
-  -s $str.fasaln -n ${FINALPREF}_RELL.$str 
+  -s $str.fasaln -n ${PREFIX}_RELL.$str 
